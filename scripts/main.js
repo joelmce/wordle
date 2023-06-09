11c4aec8d8cbfe410839704d37ebb7e8ebf004de
Joel McElroy <joelanthony.mac@gmail.com>
2023-06-09 10:10:15 +1000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,18 +10,20 @@ let game = new Game();
 
 console.log("Version: 1.1.0");
 
-/**
- * Keypress event is depreciated, using keydown instead.
- * See more: https://developer.mozilla.org/en-US/docs/Web/API/Element/keypress_event
- */
-document.addEventListener("keydown", function (e) {
-  if (!countActive) countdown(10);
+export function addEvent() {
+   /**
+   * Keypress event is depreciated, using keydown instead.
+   * See more: https://developer.mozilla.org/en-US/docs/Web/API/Element/keypress_event
+   */
+  document.addEventListener("keydown", function (e) {
+    if (!countActive) countdown(10);
 
-  countActive = true;
-  const key = e.key.toUpperCase();
-  game.listen(key);
-});
-
+    countActive = true;
+    const key = e.key.toUpperCase();
+    game.listen(key);
+  });
+ 
+}
 playAgainBtn.addEventListener("click", function () {
   wordleContainer.innerHTML = "";
   game = new Game();
